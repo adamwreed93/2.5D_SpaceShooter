481251f4001ef11a15db4d508cf25e51b4dea03f
adamwreed93 <68203005+adamwreed93@users.noreply.github.com>
2021-04-20 15:05:53 -0700

    Re-commit of the complete base game design

---
+++ 2.5D Space Shooter/Assets/Scripts/Player.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private SpawnManager _spawnManager;
+    private UIManager _uiManager;
+
+    [SerializeField] private GameObject _laserPrefab;
+    [SerializeField] private GameObject _tripleShotPrefab;
+    [SerializeField] private GameObject _shieldVisualizer;
+    [SerializeField] private GameObject _leftEngine, _rightEngine; //Similar references like this can be written like this but you hjave to be careful. This is more ofr organizational purposes.
+    [SerializeField] private AudioClip _laserSoundClip;
+
+    private AudioSource _audioSource;
+
+
+    [SerializeField] private int _score;
+    [SerializeField] private int _lives = 3;
+    [SerializeField] private float _speed = 0;
+    [SerializeField] private float _speedMultiplier = 2;
+    [SerializeField] private float _fireRate = 0;
+
+
+    private float _canFire = -1f; //The -1 starts _canFire below 0. So long as Time.time is higher than _canFire, you can shoot!  
+    private bool _isTripleShotActive = false;
+    private bool _isSpeedBoostActive = false;
+    private bool _isShieldActive = false;
+
+
+
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+        _spawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
+        _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+        _audioSource = GetComponent<AudioSource>(); 
+
+        if (_spawnManager == null)
+        {
+            Debug.LogError("The Spawn Manager is NULL.");
+        }
+
+        if (_uiManager == null)
+        {
+            Debug.LogError("The UI Manager is NULL.");
+        }
+
+        if (_audioSource == null)
+        {
+            Debug.LogError("AudioSource on the Player is NULL.");
+        }
+        else
+        {
+            _audioSource.clip = _laserSoundClip;
+        }
+    }
+
+    void Update()
+    {
+        Boundries();
+        Movement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            FireLaser();
+        }
+    }
+
+    void Boundries()
+    {
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0); //Mathf.Clamp is used here to create a min and max value for Y as it prevents the value from going any higher or lower.
+
+        if (transform.position.x >= 11.3)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11.3)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
+    }
+
+    void Movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0); //Creates the Vector3 "direction" for use within this method.
+
+        transform.Translate(direction * _speed * Time.deltaTime);
+        
+    }
+
+    void FireLaser()
+    {
+        _canFire = Time.time + _fireRate;
+
+        if (_isTripleShotActive)
+        {
+            Instantiate(_tripleShotPrefab, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 1.05f, 0), Quaternion.identity);
+        }
+
+        _audioSource.Play();
+    }
+
+
+
+    public void Damage()
+    {
+        if (_isShieldActive)
+        {
+            _isShieldActive = false;
+            _shieldVisualizer.SetActive(false); 
+            return;
+        }
+
+        _lives--;
+
+        _uiManager.UpdateLives(_lives);
+
+        if (_lives == 2)
+        {
+            _leftEngine.SetActive(true);
+        }
+        else if (_lives == 1)
+        {
+            _rightEngine.SetActive(true);
+        }
+        else if (_lives < 1)
+        {
+            _spawnManager.OnPlayerDeath();
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void TripleShotActive()
+    {
+        _isTripleShotActive = true;
+        StartCoroutine(TripleShotPowerDownRoutine());
+    }
+
+    public void SpeedBoostActive()
+    {
+        _isSpeedBoostActive = true;
+        _speed *= _speedMultiplier;
+        StartCoroutine(SpeedPowerDownRoutine());
+    }
+
+    public void ShieldActive()
+    {
+        _isShieldActive = true;
+        _shieldVisualizer.SetActive(true);
+    }
+
+    private IEnumerator TripleShotPowerDownRoutine()
+    {
+        yield return new WaitForSeconds(5.0f);
+        _isTripleShotActive = false;
+    }
+
+    private IEnumerator SpeedPowerDownRoutine()
+    {
+        yield return new WaitForSeconds(5.0f);
+        _isSpeedBoostActive = false;
+        _speed /= _speedMultiplier;
+    }
+
+    public void AddScore(int points)
+    {
+        _score += points;
+        _uiManager.UpdtaeScore(_score);
+    }
+}
---
+++ 2.5D Space Shooter/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private  GameObject _enemyPrefab;
+    [SerializeField] private GameObject[] _powerups;
+
+    [SerializeField] private GameObject _enemyContainer;
+    [SerializeField] private float spawnRate;
+
+    private bool _stopSpawning = false;
+
+   void Start()
+    {
+
+    }
+
+    public void StartSpawning()
+    {
+        StartCoroutine(SpawnEnemyRoutine());
+        StartCoroutine(SpawnPowerupRoutine());
+    }
+
+    public IEnumerator SpawnEnemyRoutine()
+    {
+        yield return new WaitForSeconds(3.0f);
+
+        while (!_stopSpawning)
+        {
+            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0); //Creates/Declares the Vector3 "posToSpawn".
+            GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity); //Creates/Declares the "newEnemy" GameObject variable and instantiates the "_enemy1Prefab" at the transform position of "posToSpawn".
+            newEnemy.transform.parent = _enemyContainer.transform; //sets the transform of the "newEnemy" GameObject to be a child of "_enemyContainer'.
+            yield return new WaitForSeconds(spawnRate);
+        }
+    }
+
+    public IEnumerator SpawnPowerupRoutine()
+    {
+        yield return new WaitForSeconds(3.0f);
+
+        while (!_stopSpawning)
+        {
+            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0); //Creates/Declares the Vector3 "posToSpawn".
+            int randomPowerUp = Random.Range(0, 3);
+            Instantiate(_powerups[randomPowerUp], posToSpawn, Quaternion.identity);
+            yield return new WaitForSeconds(Random.Range(3, 8));
+        }
+    }
+
+    public void OnPlayerDeath()
+    {
+        _stopSpawning = true;
+    }
+}
